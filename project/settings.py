8629f78bf2d80126580619b1743bc6ca9fc0ebc6
Zyppon <=>
2025-05-19 15:38:15 +0200

    init

--- project/settings.py
+++ project/settings.py
@@ -154,8 +154,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = '/static/'
 STATIC_ROOT = 'static/'
-STATIC_URL = 'static/'
+STATIC_MEDIA = '/media/'
 MEDIA_ROOT = 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
