0ed6be3d9deceeac8c4a4b460d04a31a7d32b129
Zyppon <=>
2025-05-19 11:19:17 +0200

    init

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-dm+h=(i6rrtg-__ab22rwr7xujatn%)w()+w3%jd%(g4o19&+t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
