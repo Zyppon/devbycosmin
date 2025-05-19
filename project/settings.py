e75b701425557604daa8d7a330a1ef99b8a57088
Zyppon <=>
2025-05-19 12:54:06 +0200

    init

--- project/settings.py
+++ project/settings.py
@@ -69,11 +69,11 @@ DATABASES = {
     'default': {
 
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'WyVZSWWItmJSpFoVGfVCQTitxPJSCHtF',
+        'HOST': 'tramway.proxy.rlwy.net',
+        'PORT': '25364',
     }
 }
 
